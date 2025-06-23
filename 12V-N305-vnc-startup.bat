cc2848e022673d795eeb308d2d3e7d572ce793ff
AD电气设计 <xfmniao@mail.ru>
2025-06-23 11:35:59 +0900

    Update 12V-N305-vnc-startup.bat

--- 12V-N305-vnc-startup.bat
+++ 12V-N305-vnc-startup.bat
@@ -1,2 +1,2 @@
 # bat
-@echo off REM 启动 WiFi 热点 netsh wlan set hostednetwork mode=allow ssid=MyHotspot key=12345678 netsh wlan start hostednetwork  REM 启动 TightVNC 服务 net start "TightVNC Server"
+@echo off REM 启动 WiFi 热点 netsh wlan set hostednetwork mode=allow ssid=N305 key=12345678 netsh wlan start hostednetwork  REM 启动 TightVNC 服务 net start "TightVNC Server"
